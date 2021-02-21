4402a02c456f72ab33be50f12ee198789309f09e
Vincenzo Abete <abetevincenzo@gmail.com>
2021-02-21 17:03:37 +0000

    Rewritten TCP client and server code to exlcude error there

--- src/Server.java
+++ src/Server.java
@@ -1,31 +1,32 @@
 import go_kart_go.Kart;
 import go_kart_go_network.Messages;
-import go_kart_go_network.TCPCommunicationSocket;
+import go_kart_go_network.TCPServer;
 import go_kart_go_network.UDPCommunicationSocket;
 
 
 public class Server {
 
-    TCPCommunicationSocket tcpCommunicationSocket;
+    TCPServer tcpServer;
     UDPCommunicationSocket udpCommunicationSocket;
 
     public Server(Enum protocol) {
         if (protocol.equals(Messages.Protocols.TCP)) {
-            tcpCommunicationSocket = new TCPCommunicationSocket();
+            tcpServer = new TCPServer();
         } else {
             udpCommunicationSocket = new UDPCommunicationSocket(true);
         }
     }
 
     public void listen() {
-        tcpCommunicationSocket.listen();
+        tcpServer.listen();
     }
 
     public String getMessage(Enum protocol) {
         String message;
 
         if (protocol.equals(Messages.Protocols.TCP)) {
-            message = tcpCommunicationSocket.getMessage();
+            // message = tcpCommunicationSocket.getMessage();
+            message = tcpServer.getRequest();
         } else {
             message = udpCommunicationSocket.getMessage();
         }
@@ -34,14 +35,13 @@ public class Server {
 
     public void sendMessage(String message, Enum protocol) {
         if (protocol.equals(Messages.Protocols.TCP)) {
-            tcpCommunicationSocket.sendMessage(message);
+            tcpServer.sendResponse(message);
         } else {
             udpCommunicationSocket.sendMessage(message);
         }
     }
 
     public Kart getKart() {
-        // return (Kart) udpCommunicationSocket.getObject();
-        return (Kart) tcpCommunicationSocket.getKart();
+        return (Kart) tcpServer.getObject();
     }
 }
